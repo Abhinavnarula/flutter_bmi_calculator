81930386306d53127204036b85aae714258e891c
Abhinavnarula <abhinavnarula3@gmail.com>
2020-08-29 23:46:47 +0530

    Normal Changes

---
+++ lib/constants.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+const double heigthBottomRow = 60.0;
+const Color inactiveColorValue = Color(0xFF1D1E33);
+const Color activeColorValue = Color(0xFF111328);
+const int BottomColor = 0xFFEB1555;
+
+const numberTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.w900,
+);
+
+const labelTextStyle = TextStyle(
+  color: Color(0xFF8D8EFF),
+  fontSize: 18.0,
+);
+
+const largeButtonTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 25.0,
+);
+
+const titleTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 50.0,
+);
+
+const resultTextStyle = TextStyle(
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+  color: Color(0xFF24D876),
+);
+
+const BMITextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 100.0,
+);
+
+const bodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
+
+enum Gender {
+  male,
+  female,
+}
